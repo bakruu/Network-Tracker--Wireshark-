4b56a2c190af70f3228a292e6187e75c7286bca0
Mert Ali Özçelik <bakruux@gmail.com>
2023-07-29 17:25:18 +0300

    remaster

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import pygeoip
 gi = pygeoip.GeoIP('GeoLiteCity.dat')
 
 
+def plotIPs(pcap):
+    pass
+
+
 def main():
-    f = open('wireshark_geotest.pcap', 'rb')
+    f = open('wire.pcap', 'rb')
     pcap = dpkt.pcap.Reader(f)
     kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
     '<Style id="transBluePoly">' \
@@ -19,39 +23,42 @@ def main():
     kmldoc=kmlheader+plotIPs(pcap)+kmlfooter
     print(kmldoc)
 
-def plotIPs(pcap):
-    kmlPts = ''
-    for (ts, buf) in pcap:
-        try:
-            eth = dpkt.ethernet.Ethernet(buf)
-            ip = eth.data
-            src = socket.inet_ntoa(ip.src)
-            dst = socket.inet_ntoa(ip.dst)
-            KML = retKML(dst, src)
-            kmlPts = kmlPts + KML
-        except:
-            pass
-    return kmlPts
+    def plotIPs(pcap):
+        kmlPts = ''
+        for (ts, buf) in pcap:
+            try:
+                eth = dpkt.ethernet.Ethernet(buf)
+                ip = eth.data
+                src = socket.inet_ntoa(ip.src)
+                dst = socket.inet_ntoa(ip.dst)
+                KML = retKML(dst, src)
+                kmlPts = kmlPts + KML
+            except:
+                pass
+        return kmlPts
 
-def retKML(dstip, srcip):
-    dst = gi.record_by_name(dstip)
-    src = gi.record_by_name('x.xxx.xxx.xxx')
-    try:
-        dstlongitude = dst['longitude']
-        dstlatitude = dst['latitude']
-        srclongitude = src['longitude']
-        srclatitude = src['latitude']
-        kml = (
-            '<Placemark>\n'
-            '<name>%s</name>\n'
-            '<extrude>1</extrude>\n'
-            '<tessellate>1</tessellate>\n'
-            '<styleUrl>#transBluePoly</styleUrl>\n'
-            '<LineString>\n'
-            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
-            '</LineString>\n'
-            '</Placemark>\n'
-        )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
-        return kml
-    except:
-        return ''
+    def retKML(dstip, srcip):
+        dst = gi.record_by_name(dstip)
+        src = gi.record_by_name('x.xxx.xxx.xxx')
+        try:
+            dstlongitude = dst['longitude']
+            dstlatitude = dst['latitude']
+            srclongitude = src['longitude']
+            srclatitude = src['latitude']
+            kml = (
+                      '<Placemark>\n'
+                      '<name>%s</name>\n'
+                      '<extrude>1</extrude>\n'
+                      '<tessellate>1</tessellate>\n'
+                      '<styleUrl>#transBluePoly</styleUrl>\n'
+                      '<LineString>\n'
+                      '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+                      '</LineString>\n'
+                      '</Placemark>\n'
+                  ) % (dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+            return kml
+        except:
+            return ''
+
+        if __name__ == '__main__':
+            main()
